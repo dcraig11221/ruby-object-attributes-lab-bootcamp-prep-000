ee31faf6980cbf0fd4754dd15a0914ecccd922b7
Douglas Craig <dcraig112@gmail.com>
2018-06-04 19:09:20 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,3 +12,18 @@ class Dog
     @breed
   end
 end
+
+class Person
+  def name=(persons_name)
+    @name = persons_name
+  end
+  def name
+    @name
+  end
+  def job=(persons_job)
+    @breed = persons_job
+  end
+  def job
+    @breed
+  end
+end
